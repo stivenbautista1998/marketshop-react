5da98cddde21ad72d4d689d63a8395a8906379b6
Stiven Bautista <stivenb1994@gmail.com>
2022-06-23 22:24:35 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <section>
+            <h2>Hola mundo! This is an update.</h2>
+        </section>
+    );
+}
+
+export { App };
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // ... Configuración de empaquetado
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: "development",
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        // ... Lista de reglas respecto a los loaders	
+        rules: [
+            // Reglas para babel
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            // Reglas para HTML loader
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
+}
