dec1110b79f7a3656cd56541af1cd26c91142afa
Stiven Bautista <stivenb1994@gmail.com>
2022-07-28 16:20:07 -0500

    adding styles for the refresh button

--- src/components/SyncAlert/index.jsx
+++ src/components/SyncAlert/index.jsx
@@ -3,13 +3,16 @@ import withStorageListener from './withStorageListener';
 import { Modal } from '../Modal';
 import './SyncAlert.css';
 
+import refreshIconSvg from '@icons/refresh-icon.svg';
+
 const SyncAlert = ({ showElement, onCLickHandler }) => {
     if(showElement) {
         return (
             <Modal>
                 <div className="sync-alert-wrapper">
-                    There are new changes.<br />
+                    <div className="sync-alert-text">There are new changes.</div>
                     <button onClick={onCLickHandler} className="sync-alert-wrapper__button">
+                        <img className="refresh-icon" src={refreshIconSvg} alt="refresh icon" />
                         Synchronize
                     </button>
                 </div>
@@ -22,4 +25,4 @@ const SyncAlert = ({ showElement, onCLickHandler }) => {
 
 const SyncAlertWithProps = withStorageListener(SyncAlert);
 
-export { SyncAlertWithProps };
+export { SyncAlertWithProps }; // <br />
